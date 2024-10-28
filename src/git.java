f687d250d5aea31fa409f1cffb298b07187c2908
kun <kunzhang410@gmail.com>
2024-10-28 20:58:29 +0100

    first commit

---
+++ src/git.java
@@ -0,0 +1,9 @@
+public class git {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
