ddb0c85e7ffaa2421f21f3476ed4c5c4ee96c308
kun9411 <kunzhang410@gmail.com>
2024-10-31 17:26:19 +0100

    fetch commit

--- src/git.java
+++ src/git.java
@@ -8,5 +8,6 @@ public class git {
         System.out.println("push test");
         System.out.println("push test");
          System.out.println("pull test");
+          System.out.println("fetch test");
     }
 }
